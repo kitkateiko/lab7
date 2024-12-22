b34fe4bbc8da4ef6982276aad65c12f7a169cc83
kitkateiko <tyrkov2003@mail.ru>
2024-12-22 23:55:19 +0700

    Подготовка RecyclerView к выводу изображений / Подготовка к загрузке через URL

--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -1,6 +1,10 @@
 package api
+
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
+
 interface FlickrApi {
     @GET(
         "services/rest/?method=flickr.interestingness.getList" +
@@ -10,4 +14,7 @@ interface FlickrApi {
                 "&extras=url_s"
     )
     fun fetchPhotos(): Call<FlickrResponse>
+
+    @GET
+    fun fetchUrlBytes(@Url url: String) : Call<ResponseBody>
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
@@ -8,7 +8,11 @@ import api.PhotoResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.annotation.WorkerThread
 import retrofit2.Retrofit
+import okhttp3.ResponseBody
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -48,4 +52,13 @@ class FlickrFetchr {
         })
         return responseLiveData
     }
+
+    @WorkerThread
+    fun fetchPhoto(url: String): Bitmap? {
+        val response: Response<ResponseBody> = flickrApi.fetchUrlBytes(url).execute()
+        val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decoded bitmap=$bitmap from Response=$response")
+        return bitmap
+    }
+
 }
